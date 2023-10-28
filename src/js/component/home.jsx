ac0da7f97ce00ceaeab204792d84b62217859956
JorgeOteiza <oteiza.jor@gmail.com>
2023-10-28 03:20:38 -0300

    proyecto semáforo terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,35 +1,52 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-//create your first component
 const Home = () => {
+	const colors = [
+		{ name: 'rojo', color: 'red' },
+		{ name: 'amarillo', color: 'yellow' },
+		{ name: 'verde', color: 'green' }
+	];
 
-	const colors = [{
-		name: "rojo",
-		color: "red"
-	}, {
-		name: "amarillo",
-		color: "yellow"
-	}, {
-		name: "verde",
-		color: "green"
-	}]
+	const [selected, setSelected] = useState(null);
 
-	const [selected, setSelected] = useState({})
+	const handleClick = (el) => {
+		if (selected && selected.name === el.name) {
+			setSelected(null); // Apagar la luz si se hace clic en un color ya seleccionado
+		} else {
+			setSelected(el);
+		}
+	};
+
 	return (
-		<section style={{ width: "max-content", padding: "100px", margin: "auto", backgroundColor: "black", }}>
-			{
-				colors.map((el) => (<article>
-					<button onClick={() => setSelected(el)} style={{
-						borderRadius: "50%",
-						width: "150px",
-						height: "150px",
-						fontWeight: "600",
-						backgroundColor: el.color,
-						border: el.name === selected.name ? '4px solid lightblue' : 'none'
-					}}></button>
-				</article>
-				))
-			}
+		<section style={{
+			width: 'max-content',
+			padding: '21px',
+			margin: 'auto',
+			marginTop: '99px',
+			backgroundColor: 'black',
+			position: 'relative',
+			borderRadius: '30px',
+		}}>
+			<div style={{ position: 'absolute', top: '-99px', left: '50%', transform: 'translateX(-50%)', width: '21px', height: '99px', backgroundColor: 'black' }}></div>
+			<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+				{colors.map((el) => (
+					<article key={el.name} style={{ marginBottom: '20px' }}>
+						<button
+							onClick={() => handleClick(el)}
+							style={{
+								borderRadius: '50%',
+								width: '96px',
+								height: '96px',
+								fontWeight: '600',
+								backgroundColor: el.color,
+								border: selected && el.name === selected.name ? '0 solid lightblue' : 'none',
+								boxShadow: selected && el.name === selected.name ? '0 0 60px 50px rgba(173, 216, 230, 1.0)' : 'none',
+								transition: 'box-shadow 0.3s'
+							}}
+						></button>
+					</article>
+				))}
+			</div>
 		</section>
 	);
 };
